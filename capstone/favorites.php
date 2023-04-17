a5126e6ec45079f547d871fccd0f39bf4aff1b9f
sunRyzen <44350437+sunRyzen@users.noreply.github.com>
2023-04-17 16:29:27 -0400

    created favorites
    added favorites, only shows my current favorites from movies and tv shows. still needs jackies and sandeeps code

---
+++ capstone/favorites.php
@@ -0,0 +1,54 @@
+<link rel="stylesheet" type="text/css" href="styles2.css">
+
+<?php
+
+//db connection to main db
+  $host = "sql9.freemysqlhosting.net";
+  $username = "sql9611840";
+  $password = "n74TrMNk2r";
+  $database = "sql9611840";
+
+  $conn = new mysqli($host, $username, $password, $database);
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+//selects and outputs from movies table
+  $sql = "SELECT * FROM movies";
+  $result = mysqli_query($conn, $sql);
+
+  echo "<h1 style='font-size: 48px;'>Movies</h1>";
+  if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      echo "<h2>" . $row["title"] . "</h2>";
+      echo "<p>" . $row["overview"] . "</p>";
+      echo "<p>Release Date: " . $row["release_date"] . "</p>";
+      echo "<p>Popularity: " . $row["popularity"] . "</p>";
+      echo "<hr>";
+    }
+  } else {
+    echo "No movies found.";
+  }
+
+//selects and outputs from tv_shows table
+  $sql = "SELECT * FROM tv_shows";
+  $result = mysqli_query($conn, $sql);
+
+  echo "<h1 style='font-size: 48px;'>TV Shows</h1>";
+  if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      echo "<h2>" . $row["name"] . "</h2>";
+      echo "<p>" . $row["overview"] . "</p>";
+      echo "<p>First Air Date: " . $row["first_air_date"] . "</p>";
+      echo "<p>Popularity: " . $row["popularity"] . "</p>";
+      echo "<hr>";
+    }
+  } else {
+    echo "No TV shows found.";
+  }
+
+  //start itunes and books code below. all you need to do is print out the results saved in your db. no need to make an extra connection, its already connected.
+
+  $conn->close();
+?>
