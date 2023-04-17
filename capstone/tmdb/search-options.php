8be63f33c0d689c267d7070e3564bdeba25ddf35
sunRyzen <44350437+sunRyzen@users.noreply.github.com>
2023-04-17 16:22:15 -0400

    Update search-options.php
    added css

--- capstone/tmdb/search-options.php
+++ capstone/tmdb/search-options.php
@@ -2,21 +2,29 @@
 <html>
   <head>
     <title>Media Search</title>
+        <button onclick="goBack()">Back</button>
+
+<script>
+function goBack() {
+  window.location.href = "/~umanaj47";
+}
+</script>
+        <link rel="stylesheet" type="text/css" href="styles2.css">
   </head>
   <body>
     <h1>Media Search</h1>
     <a href="movie-search.php">Movie Search</a>
     <br>
-	<br>
+        <br>
     <a href="tv-search.php">TV Show Search</a>
-	<br>
-	<br>
+        <br>
+        <br>
     <a href="top-movies.php">Popular Recent Movies</a>
-	<br>
-	<br>
+        <br>
+        <br>
     <a href="top-shows.php">Popular Recent Shows</a>
-	<br>
-	<br>
-    <a href="watch-later.php">Watch Later</a>
+        <br>
+        <br>
+    <a href="search-history.php">Search History</a>
   </body>
 </html>
