fcaf9bdb673ea457c84c806cceb05e331ffca568
sunRyzen <44350437+sunRyzen@users.noreply.github.com>
2023-04-17 16:24:31 -0400

    Update tv.php
    added css and back button

--- capstone/tmdb/tv.php
+++ capstone/tmdb/tv.php
@@ -7,17 +7,16 @@
   if (isset($_GET['id'])) {
     $tv = $tmdb->getTVShow($_GET['id']);
   }
-  
+
   //above gets the TMDB wrapper, calls it to get the movie by id.
   //below starts the database connection
-  
-   if (isset($_POST['save'])) {
-    $host = "sql9.freemysqlhosting.net";
-    $username = "sql9608271";
-    $password = "GNMjTMrfNs";
-    $database = "sql9608271";
-	//make this into an include
 
+   if (isset($_POST['save'])) {
+
+  $host = "sql9.freemysqlhosting.net";
+  $username = "sql9611840";
+  $password = "n74TrMNk2r";
+  $database = "sql9611840";
     $conn = new mysqli($host, $username, $password, $database);
 
     if ($conn->connect_error) {
@@ -46,6 +45,14 @@
 <html>
   <head>
     <title>TV Show Information</title>
+        <link rel="stylesheet" type="text/css" href="styles2.css">
+        <button onclick="goBack()">Back</button>
+
+<script>
+function goBack() {
+  window.location.href = "search-options.php";
+}
+</script>
   </head>
   <body>
     <?php if (isset($tv)): ?>
@@ -56,8 +63,8 @@
         <li>Release Date: <?php echo $tv->getReleaseDate(); ?></li>
         <li>Popularity: <?php echo $tv->getPopularity(); ?></li>
       </ul>
-	  <form action="tv.php?id=<?php echo $_GET['id']; ?>" method="post">
-        <input type="submit" name="save" value="Save TV Show Data">
+          <form action="tv.php?id=<?php echo $_GET['id']; ?>" method="post">
+        <input type="submit" name="save" value="Add to Favorites">
       </form>
     <?php else: ?>
       <h1>Show not found</h1>
