003eddc01b6439ccf7c6386dcd8117d64957a739
sunRyzen <44350437+sunRyzen@users.noreply.github.com>
2023-04-17 16:23:51 -0400

    Update tv-search.php
    added css + search history

--- capstone/tmdb/tv-search.php
+++ capstone/tmdb/tv-search.php
@@ -3,10 +3,28 @@
 
   $tmdb = new TMDB();
   $tmdb->setAPIKey('21855c98a465091b126f54446a358b4a');
+  //api key set and db connect
+  $host = "sql9.freemysqlhosting.net";
+  $username = "sql9611840";
+  $password = "n74TrMNk2r";
+  $database = "sql9611840";
+
+  $conn = new mysqli($host, $username, $password, $database);
 
   if (isset($_POST['search'])) {
     $query = $_POST['query'];
     $tvs = $tmdb->searchTVShow($query);
+
+        //add top 5 search results to search-history (inside the isset)
+        $stmt = $conn->prepare('INSERT INTO `search-history` (title, search_date, media_type) VALUES (?, ?, ?)');
+        for($i =0; $i < 5 && isset($tvs[$i]); $i++)
+        {
+    $title = $tvs[$i]->getName();
+    $search_date = date('Y-m-d H:i:s');
+        $media_type = 'Show';
+    $stmt->bind_param('sss', $title, $search_date, $media_type);
+    $stmt->execute();
+        }
   }
 
   if (isset($_GET['id'])) {
@@ -18,6 +36,14 @@
 <html>
   <head>
     <title>TV Search</title>
+        <link rel="stylesheet" type="text/css" href="styles2.css">
+        <button onclick="goBack()">Back</button>
+
+<script>
+function goBack() {
+  window.location.href = "search-options.php";
+}
+</script>
   </head>
   <body>
     <h1>TV Search</h1>
