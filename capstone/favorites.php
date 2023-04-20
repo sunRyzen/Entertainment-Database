cc8fb981beca33044cbb8062e8b1755b4f2d4c06
SandeepReji <70913465+SandeepReji@users.noreply.github.com>
2023-04-20 18:01:07 -0400

    Update favorites.php

--- capstone/favorites.php
+++ capstone/favorites.php
@@ -51,6 +51,50 @@
 echo "<h1 style='font-size: 48px;'>Books</h1>";
   //start books code below. all you need to do is print out the results saved in your db. no need to make an extra connection, its already connected.
 
+  if (isset($_GET['search_query'])) {
+      $search_query = $_GET['search_query'];
+  } else {
+      $search_query = "";
+  }
+  
+  // Step 3: Perform the search
+  if (isset($_GET['search'])) {
+      $query = "SELECT * FROM book WHERE title LIKE '%$search_query%' and fav = 1";
+      $result = mysqli_query($conn, $query);
+  } else {
+      $query = "SELECT * FROM book where fav = 1" ;
+      $result = mysqli_query($conn, $query);
+  
+  }
+  $i = 0;
+  // Step 4: Display the results
+  if ($result && mysqli_num_rows($result) > 0) {
+  
+      echo "<table border='1'>";
+      echo "<tr>
+              <th>Sr</th>
+              <th>Thumbnail</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Publisher</th>
+              <th>Search date</th
+              ></tr>";
+      while ($row = mysqli_fetch_assoc($result)) {
+          $i++;
+          echo "<tr>";
+          echo "<td>".$i."</td>";
+          echo "<td><img src='".$row['thumb']."' width='100pc' height='100'/></td>";
+          echo "<td>" . $row['title'] . "</td>";
+          echo "<td>" . $row['author'] . "</td>";
+          echo "<td>" . $row['pub'] . "</td>";
+          echo "<td>" . $row['date'] . "</td>";
+          echo "</tr>";
+      }
+      echo "</table>";
+  } else {
+      echo "No search results found.";
+  }
+
 echo "<h1 style='font-size: 48px;'>Songs</h1>";
   //start music code below. all you need to do is print out the results saved in your db. no need to make an extra connection, its already connected.
   // Songs
