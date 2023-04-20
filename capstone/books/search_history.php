9fbf8e33bb61564816034e0ea061eb7c0386f266
SandeepReji <70913465+SandeepReji@users.noreply.github.com>
2023-04-20 16:38:23 -0400

    Update search_history.php

--- capstone/books/search_history.php
+++ capstone/books/search_history.php
@@ -36,18 +36,19 @@ th {
 }
 </style>
 
+
 <!-- search form -->
 <form method="get" action="">
   <input type="text" name="search_query" placeholder="Search by title">
   <button type="submit" value="yes" name="search">Search</button>
 </form>
-
 <?php
 // Step 1: Connect to the database
 $conn = mysqli_connect("sql9.freemysqlhosting.net", "sql9611840", "n74TrMNk2r", "sql9611840", 3306);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
 // Step 2: Get the search query
 if (isset($_GET['search_query'])) {
     $search_query = $_GET['search_query'];
@@ -57,24 +58,26 @@ if (isset($_GET['search_query'])) {
 
 // Step 3: Perform the search
 if (isset($_GET['search'])) {
-    $query = "SELECT * FROM book WHERE title LIKE '%$search_query%' ORDER BY date DESC LIMIT 50";
+    $query = "SELECT * FROM book WHERE title LIKE '%$search_query%'";
     $result = mysqli_query($conn, $query);
 } else {
-    $query = "SELECT * FROM book ORDER BY date DESC LIMIT 50";
+    $query = "SELECT * FROM book";
     $result = mysqli_query($conn, $query);
+
 }
 $i = 0;
 // Step 4: Display the results
 if ($result && mysqli_num_rows($result) > 0) {
-    echo "<table>";
+
+    echo "<table border='1'>";
     echo "<tr>
-    <th>Sr</th>
-    <th>Thumbnail</th>
-    <th>Title</th>
-    <th>Author</th>
-    <th>Publisher</th>
-    <th>Search date</th>
-    </tr>";
+            <th>Sr</th>
+            <th>Thumbnail</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Publisher</th>
+            <th>Search date</th
+            ></tr>";
     while ($row = mysqli_fetch_assoc($result)) {
         $i++;
         echo "<tr>";
