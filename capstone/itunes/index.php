8e68486b93155334418f25758bbf3d64f8f805a0
Jzheng2001 <98425462+Jzheng2001@users.noreply.github.com>
2023-04-21 12:29:51 -0400

    Update index.php

--- capstone/itunes/index.php
+++ capstone/itunes/index.php
@@ -10,6 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -21,7 +22,7 @@ if ($conn->connect_error) {
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-	
+
     <style>
         h1 {
             text-align: center;
@@ -52,55 +53,29 @@ if ($conn->connect_error) {
             box-sizing: border-box;
         }
 
-        .light-mode {
-            background-color: white;
-            color: #000;
+        button {
+            border-radius: 5px;
         }
 
-        .light-mode input[type=text], .light-mode input[type=submit] {
-            border: 1px solid #333;
-        }
+        .back-btn {
+            position: absolute;
+            left: 5px;
+            top: 5px;
 
-        .light-mode table thead th {
-            background-color: #f8f9fa;
-            color: #000;
         }
-
-        .light-mode .text-danger {
-            color: red;
-		}
-		button {
-			border-radius: 10px;
-		}
-		
-		.back-btn {
-			position: absolute;
-			left: 5px;
-			top: 5px;
-	    }
 
     </style>
-	<button class="back-btn" onclick="goBack()">Back</button>
-		
-		<script>
-	function goBack() {
-	  window.location.href = "http://elvis.rowan.edu/~umanaj47/";
-	}
-	</script>
+        <button class="back-btn" onclick="goBack()">Back to Entertainment Database</button>
+
+                <script>
+        function goBack() {
+          window.location.href = "http://elvis.rowan.edu/~umanaj47/";
+        }
+        </script>
 </head>
 
 <body>
 <div class="container">
-    <div class="light-mode-toggle" style="position:fixed; top:50%; right:0px;">
-        <button onclick="lightMode();">Toggle Light Mode</button>
-    </div>
-    <script>
-        function lightMode() {
-            var element = document.body;
-            element.classList.toggle("light-mode");
-        }
-    </script>
-
     <h1>Song Search</h1>
 
     <form action="" method="post">
@@ -111,89 +86,103 @@ if ($conn->connect_error) {
     <div class="container my-5">
         <table class="table">
             <?php
-				if (isset($_POST['submit'])) {
-					$search = $_POST['search'];
+                                if (isset($_POST['submit'])) {
+                                        $search = $_POST['search'];
 
-					// query to search for songs
-					$sql = "SELECT * FROM itunesapi WHERE artist_name LIKE '%$search%' OR
-							track_name LIKE '%$search%' OR release_date LIKE '%$search%' OR explicitness LIKE
-							'%$search%' OR primary_genre LIKE '%$search%'";
-					$result = mysqli_query($conn, $sql);
+                                        // query to search for songs
+                                        $sql = "SELECT * FROM itunesapi WHERE artist_name LIKE '%$search%' OR
+                                                        track_name LIKE '%$search%' OR release_date LIKE '%$search%' OR explicitness LIKE
+                                                        '%$search%' OR primary_genre LIKE '%$search%'";
+                                        $result = mysqli_query($conn, $sql);
+														
+                                        if ($result) {
+                                                if (mysqli_num_rows($result) > 0) {
+                                                        echo '<table class="table">
+                                                                          <thead>
+                                                                                  <tr>
+                                                                                          <th>Artist Name</th>
+                                                                                          <th>Track Name</th>
+                                                                                          <th>Release Date</th>
+                                                                                          <th>Explicitness</th>
+                                                                                          <th>Primary Genre</th>
+                                                                                          <th>Link URL</th>
+                                                                                          <th>Add to Favorites</th>
+                                                                                  </tr>
+                                                                          </thead>
+                                                                          <tbody>';
 
-					if ($result) {
-						if (mysqli_num_rows($result) > 0) {
-							echo '<table class="table">
-									  <thead>
-										  <tr>
-											  <th>Artist Name</th>
-											  <th>Track Name</th>
-											  <th>Release Date</th>
-											  <th>Explicitness</th>
-											  <th>Primary Genre</th>
-											  <th>Link URL</th>
-											  <th>Add to Favorites</th>
-										  </tr>
-									  </thead>
-									  <tbody>';
+                                                        while($row = mysqli_fetch_assoc($result)) {
+                                                                echo '<tr>
+                                                                                  <td>'.$row['artist_name'].'</td>
+                                                                                  <td>'.$row['track_name'].'</td>
+                                                                                  <td>'.$row['release_date'].'</td>
+                                                                                  <td>'.$row['explicitness'].'</td>
+                                                                                  <td>'.$row['primary_genre'].'</td>
+                                                                                  <td><a href="'.$row['track_url'].'" class="btn btn-outline-primary">Open in iTunes</a></td>
+                                                                                  <td>
+                                                                                          <form action="" method="post">
+                                                                                                  <input type="hidden" name="artist_name" value="'.$row['artist_name'].'">
+                                                                                                  <input type="hidden" name="track_name" value="'.$row['track_name'].'">
+                                                                                                  <input type="hidden" name="release_date" value="'.$row['release_date'].'">
+                                                                                                  <input type="hidden" name="explicitness" value="'.$row['explicitness'].'">
+                                                                                                  <input type="hidden" name="primary_genre" value="'.$row['primary_genre'].'">
+                                                                                                  <input type="hidden" name="track_url" value="'.$row['track_url'].'">
+                                                                                                  <button class="btn btn-outline-danger" name="add_to_favorites">Add to Favorites</button>
+                                                                                          </form>
+                                                                                  </td>
+                                                                          </tr>';
+										 $artist_name = mysqli_real_escape_string($conn, $row['artist_name']);
+										 $track_name = mysqli_real_escape_string($conn, $row['track_name']);
+									         $release_date = mysqli_real_escape_string($conn, $row['release_date']);
+										 $explicitness = mysqli_real_escape_string($conn, $row['explicitness']);
+										 $primary_genre = mysqli_real_escape_string($conn, $row['primary_genre']);
+										 $track_url = mysqli_real_escape_string($conn, $row['track_url']);
+										 $select_sql = "SELECT * FROM ssearch_history WHERE artist_name = '$artist_name' AND track_name = '$track_name' AND release_date = '$release_date' AND explicitness = '$explicitness' AND primary_genre = '$primary_genre' AND track_url = '$track_url'";
+										 $select_result = mysqli_query($conn, $select_sql);
+										 if(mysqli_num_rows($select_result) == 0) {
+										 // Record does not exist, insert it
+										 $insert_sql = "INSERT INTO ssearch_history (artist_name, track_name, release_date, explicitness, primary_genre, track_url)
+											          VALUES ('$artist_name', '$track_name', '$release_date', '$explicitness', '$primary_genre', '$track_url')";
+										 mysqli_query($conn, $insert_sql);
+}
+                                                        }
 
-							while($row = mysqli_fetch_assoc($result)) {
-								echo '<tr>
-										  <td>'.$row['artist_name'].'</td>
-										  <td>'.$row['track_name'].'</td>
-										  <td>'.$row['release_date'].'</td>
-										  <td>'.$row['explicitness'].'</td>
-										  <td>'.$row['primary_genre'].'</td>
-										  <td><a href="'.$row['track_url'].'" class="btn btn-outline-primary">Open in iTunes</a></td>
-										  <td>
-											  <form action="" method="post">
-												  <input type="hidden" name="artist_name" value="'.$row['artist_name'].'">
-												  <input type="hidden" name="track_name" value="'.$row['track_name'].'">
-												  <input type="hidden" name="release_date" value="'.$row['release_date'].'">
-												  <input type="hidden" name="explicitness" value="'.$row['explicitness'].'">
-												  <input type="hidden" name="primary_genre" value="'.$row['primary_genre'].'">
-												  <input type="hidden" name="track_url" value="'.$row['track_url'].'">
-												  <button class="btn btn-outline-primary" name="add_to_favorites">Add to Favorites</button>
-											  </form>
-										  </td>
-									  </tr>';
-							}
+                                                        echo '</tbody></table>';
+                                                } else {
+                                                        echo '<h2 class="text-danger">Song not found.</h2>';
+                                                }
+                                        }
+                                }
 
-							echo '</tbody></table>';
-						} else {
-							echo '<h2 class="text-danger">Song not found.</h2>';
-						}
-					}
-				}
+                                if (isset($_POST['add_to_favorites'])) {
+                                        $artist_name = $_POST['artist_name'];
+                                        $track_name = $_POST['track_name'];
+                                        $release_date = $_POST['release_date'];
+                                        $explicitness = $_POST['explicitness'];
+                                        $primary_genre = $_POST['primary_genre'];
+                                        $track_url = $_POST['track_url'];
 
-				if (isset($_POST['add_to_favorites'])) {
-					$artist_name = $_POST['artist_name'];
-					$track_name = $_POST['track_name'];
-					$release_date = $_POST['release_date'];
-					$explicitness = $_POST['explicitness'];
-					$primary_genre = $_POST['primary_genre'];
-					$track_url = $_POST['track_url'];
+                                        // query to insert song into favorites table
+                                        $sql = "SELECT COUNT(*) FROM favoritesong WHERE track_url = '$track_url'";
+                                        $result = mysqli_query($conn, $sql);
+                                        $count = mysqli_fetch_array($result)[0];
 
-					// query to insert song into favorites table
-					$sql = "SELECT COUNT(*) FROM favoritesong WHERE track_url = '$track_url'";
-					$result = mysqli_query($conn, $sql);
-					$count = mysqli_fetch_array($result)[0];
-
-					if ($count > 0) {
-					  echo '<div class="alert alert-warning" role="alert">Song is already in favorites!</div>';
-					} else {
-					  // query to insert song into favorites table
-					  $sql = "INSERT INTO favoritesong (artist_name, track_name, release_date, explicitness, primary_genre, track_url)
-							  VALUES ('$artist_name', '$track_name', '$release_date', '$explicitness', '$primary_genre', '$track_url')";
-					  if (mysqli_query($conn, $sql)) {
-						echo '<div class="alert alert-success" role="alert">Song added to favorites!</div>';
-					  } else {
-						echo '<div class="alert alert-danger" role="alert">Error adding song to favorites.</div>';
-					  }
-					}
-				}
-					?>
+                                        if ($count > 0) {
+                                          echo '<div class="alert alert-warning" role="alert">Song is already in favorites!</div>';
+                                        } else {
+                                          // query to insert song into favorites table
+                                          $sql = "INSERT INTO favoritesong (artist_name, track_name, release_date, explicitness, primary_genre, track_url)
+                                                          VALUES ('$artist_name', '$track_name', '$release_date', '$explicitness', '$primary_genre', '$track_url')";
+                                          if (mysqli_query($conn, $sql)) {
+                                                echo '<div class="alert alert-success" role="alert">Song added to favorites!</div>';
+                                          } else {
+                                                echo '<div class="alert alert-danger" role="alert">Error adding song to favorites.</div>';
+                                          }
+                                        }
+                                }
+                                        ?>
         </table>
   </div>
   </div>
   </body>
-  </html>
+</html>
