758077f707a395b6ccd0658483a4bf18a3bcda7f
sunRyzen <44350437+sunRyzen@users.noreply.github.com>
2023-04-24 17:23:17 -0400

    Update movie-search.php

--- capstone/tmdb/movie-search.php
+++ capstone/tmdb/movie-search.php
@@ -38,21 +38,87 @@
 <html>
   <head>
     <title>Movie Search</title>
-        <link rel="stylesheet" type="text/css" href="styles2.css">
-        <button onclick="goBack()">Back</button>
+    <style>
+          body {
+                background-color: #333;
+          }
+          h1, h2 {
+                color: white;
+          }
+          a {
+                color: white;
+                text-decoration: none;
+          }
+          a:visited {
+                color: white;
+          }
+          a:hover, a:active {
+                color: white;
+                text-decoration: underline;
+          }
 
-<script>
-function goBack() {
-  window.location.href = "search-options.php";
-}
-</script>
+      .header-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 10vh;
+      }
+
+          .back-button {
+        position: absolute;
+        left: 0;
+        top: 0;
+      }
+
+      .search-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 20px;
+      }
+
+      .button-container {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+      }
+
+          .green-button {
+                background-color: green;
+                color: white;
+          }
+
+          .results-container {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+          }
+
+        .header-button-container {
+        position: absolute;
+        top: 0;
+        right: 0;
+        margin: 10px;
+      }
+    </style>
   </head>
   <body>
-    <h1>Movie Search</h1>
-    <form action="" method="post">
-      <input type="text" name="query">
-      <input type="submit" value="Search" name="search">
-    </form>
+    <div class="header-container">
+      <h1>Movie Search</h1>
+      <a href="http://elvis.rowan.edu/~umanaj47/" class="back-button"><button>Back</button></a>
+          <a href="search-history.php" class="header-button-container"><button>View Search History</button></a>
+    </div>
+    <div class="search-container">
+      <form action="" method="post">
+        <input type="text" name="query">
+        <input type="submit" value="Search" name="search">
+      </form>
+      <div class="button-container">
+        <a href="tv-search.php"><button class="green-button">Swap to TV Search</button></a>
+        <a href="top-movies.php"><button class="green-button">Show Popular Movies</button></a>
+      </div>
+    </div>
+        <div class="results-container">
     <?php if (isset($movies)): ?>
       <h2>Results</h2>
       <ul>
@@ -65,5 +131,6 @@ function goBack() {
         <?php endforeach; ?>
       </ul>
     <?php endif; ?>
+        </div>
   </body>
 </html>
