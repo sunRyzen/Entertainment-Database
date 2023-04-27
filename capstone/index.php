02581e4f698241892f4bcf80efcc69b0493be68b
sunRyzen <44350437+sunRyzen@users.noreply.github.com>
2023-04-27 19:35:45 -0400

    Update index.php

--- capstone/index.php
+++ capstone/index.php
@@ -2,16 +2,15 @@
 <html>
 <head>
   <title>Welcome to the Entertainment Database</title>
-  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="stylesheet" type="text/css" href="styles2.css">
 </head>
 <body>
   <h1>Welcome to the Entertainment Database</h1>
   <ul>
-    <li><a href="tmdb/search-options.php">The Movie Database</a></li>
-    <li><a href="itunes/something.php">iTunes Lookup (still needs to be added)<>
-    <li><a href="http://elvis.rowan.edu/~rejisa33/">Book Lookup (still needs to be added)</a>>
-    <li><a href="favorites.php">Favorites Page</a></li>
+    <li><a href="tmdb/movie-search.php">Movie/Show Lookup</a></li><br><br>
+    <li><a href="http://elvis.rowan.edu/~zhengj29/">Music Lookup</a></li><br><br>
+    <li><a href="http://elvis.rowan.edu/~rejisa33/">Book Lookup</a></li><br><br>
+    <li><a href="favorites.php">Favorites Page</a></li><br>
   </ul>
 </body>
 </html>
-
